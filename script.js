eeffd1df8482c184eb19bcc5c1e668e3d4b3bcfd
vignesh448 <vigneshsubramani8@gmail.com>
2026-06-18 12:36:05 +0530

    first task

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var EmployeeName = "Vignesh";
+let EmployeeAge = 26;
+const CompanyName = "Stackly";
+
+console.log("Employee Name :", EmployeeName);
+console.log( "Employee Age:", EmployeeAge);
+console.log("Company Name:", CompanyName);
+
+// 5. Show a popup message:
+alert("Welcome to Employee Portal");
+
+// 6. Ask the employee:
+
+let taskCompleted = confirm("Have You completed today`s task");
+console.log("taskCompleted");
+
+// 7. Ask the employee name using prompt().
+var employeeName = prompt("Enter your Name");
+console.log("Employee Name :" + employeeName);
+
+// 8. Display the entered employee name on the webpage.
+
+document.writeln("Welcome" + employeeName );
+
+// 9. Print a success message using console.warn().
+ console.warn("Task submitted Successfully");
+
+ // 10. Print an error message using console.error().
+ console.error("Task Submission Failed");
+
+ // Bonus Task:
+ EmployeeAge = 27;
+ console.log("Updated Employee Age:", EmployeeAge);
